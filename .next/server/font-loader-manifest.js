1911814c8a1c267e99be69da104a9ae863eefd2b
Ibrahim Nkya <ibra.isn@gmail.com>
2023-09-17 23:22:52 +0300

    Bug Fix

---
+++ .next/server/font-loader-manifest.js
@@ -0,0 +1,8 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {},
+  "app": {
+    "/ISN STUDIO/extentadvisory.co.tz/site3/node_modules/@next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/c9a5bc6a7c948fb0.p.woff2"
+    ]
+  }
+}
